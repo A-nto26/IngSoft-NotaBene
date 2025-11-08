d8c8278fca4d7b31739b28d77546efc7e94820b7
A-nto26 <anto.candia00@gmail.com>
2025-11-08 15:36:04 +0100

    set up progetto

---
+++ backend/src/main/java/com/sweng/notes/NoteBackendApplication.java
@@ -0,0 +1,12 @@
+package com.sweng.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteBackendApplication.class, args);
+    }
+}
